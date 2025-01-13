99c827ba26df71f31befe3015240817857558c40
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-13 08:13:24 +0700

    migrasi table Books

---
+++ database/migrations/2025_01_13_005140_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('author');
+            $table->string('isbn');
+            $table->string('cover');
+            $table->text('description');
+            $table->integer('quantity')->default(0);
+            $table->integer('available')->default(0);
+            $table->year('year');
+            $table->string('publisher');
+            $table->string('language');
+            $table->string('category');
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
