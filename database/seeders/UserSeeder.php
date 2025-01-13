43e3649a6d824b71ec4d75134410639556505b31
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-13 07:32:26 +0700

    membuat seeder untuk table users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'ilmannafi',
+            'email' => 'ilman@gmail.com',
+            'password' => bcrypt('12345678'),
+            'address' => 'Jl. Raya No. 1',
+            'role' => 'admin',
+            'phone' => '08123456789',
+        ]);
+    }
+}
