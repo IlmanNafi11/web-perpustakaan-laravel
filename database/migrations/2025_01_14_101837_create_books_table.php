33006f631190a15f1977e93c0959e487600a7f98
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-14 17:46:32 +0700

    membuat migrasi baru untuk table books

---
+++ database/migrations/2025_01_14_101837_create_books_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Livewire\on;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('author');
+            $table->string('isbn');
+            $table->string('cover');
+            $table->text('description');
+            $table->integer('quantity')->default(0);
+            $table->integer('available')->default(0);
+            $table->year('year');
+            $table->string('publisher');
+            $table->string('language');
+            $table->string('type');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('category')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
