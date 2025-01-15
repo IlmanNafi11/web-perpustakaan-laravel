184e8428c94ec92135f44baa3d65282ae4ca8a60
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-15 23:14:12 +0700

    membuat model member

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'nik',
+        'status',
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
-    }
+}
 }
