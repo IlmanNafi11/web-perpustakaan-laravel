708f5ab6c3b739e5494b5bd055023520f70c6ff8
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-16 23:13:42 +0700

    membuat page view untuk detail borrow request

---
+++ app/Filament/Resources/BorrowRequestResource/Pages/viewBorrowRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BorrowRequestResource\Pages;
+
+use App\Filament\Resources\BorrowRequestResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class viewBorrowRequest extends ViewRecord
+{
+    protected static string $resource = BorrowRequestResource::class;
+}
