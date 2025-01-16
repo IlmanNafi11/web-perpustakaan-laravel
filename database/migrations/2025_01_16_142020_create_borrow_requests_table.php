61ff0b200839c4cef407c12761ab47dbc3ba47b6
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-16 21:40:22 +0700

    Membuat migrasi table borrow request

---
+++ database/migrations/2025_01_16_142020_create_borrow_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrow_requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('member_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('book_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('status')->default('pending');
+            $table->integer('quantity')->default(1);
+            $table->timestamp('request_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('processed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrow_requests');
+    }
+};
