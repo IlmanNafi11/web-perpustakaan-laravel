1b78f07ad95715a6beacb7d372123268b1e8360f
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-16 20:31:11 +0700

    memperbarui pesan notifikasi

--- app/Filament/Resources/BookResource/Pages/CreateBook.php
+++ app/Filament/Resources/BookResource/Pages/CreateBook.php
@@ -5,13 +5,22 @@ namespace App\Filament\Resources\BookResource\Pages;
 use App\Filament\Resources\BookResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateBook extends CreateRecord
 {
     protected static string $resource = BookResource::class;
 
     protected function getRedirectUrl(): string
-{
-    return $this->getResource()::getUrl('index');
-}
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return Notification::make()
+            ->success()
+            ->title('Succeed')
+            ->body('Data saved successfully');
+    }
 }
--- app/Filament/Resources/BookResource/Pages/EditBook.php
+++ app/Filament/Resources/BookResource/Pages/EditBook.php
@@ -5,15 +5,22 @@ namespace App\Filament\Resources\BookResource\Pages;
 use App\Filament\Resources\BookResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
 
 class EditBook extends EditRecord
 {
     protected static string $resource = BookResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getSavedNotification(): ?Notification
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        return Notification::make()
+            ->success()
+            ->title('Succeed')
+            ->body('The book was updated successfully');
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->previousUrl ?? $this->getResource()::getUrl('index');
     }
 }
