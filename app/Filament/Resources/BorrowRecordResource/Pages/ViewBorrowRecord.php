4c49d53943fd94017e5184dcd2308ab449767e20
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-18 00:05:17 +0700

    membuat view borrow record resource

---
+++ app/Filament/Resources/BorrowRecordResource/Pages/ViewBorrowRecord.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\BorrowRecordResource\Pages;
+
+use App\Filament\Resources\BorrowRecordResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewBorrowRecord extends ViewRecord
+{
+    protected static string $resource = BorrowRecordResource::class;
+}
