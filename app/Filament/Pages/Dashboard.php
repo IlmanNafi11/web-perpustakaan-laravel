d103552443b78e24e9fbaa430403ef97c4c5cba5
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-19 21:06:03 +0700

    mengatur jumlah kolom dashboard

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -5,4 +5,9 @@ namespace App\Filament\Pages;
 class Dashboard extends \Filament\Pages\Dashboard
 {
     protected static ?string $title = 'Dashboard';
+
+    public function getColumns(): int | string | array
+    {
+        return 2;
+    }
 }
