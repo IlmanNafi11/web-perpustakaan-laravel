2d3f63190f45613afad99f0750411c87412bc720
IlmanNafi11 <ilmannafi04@gmail.com>
2025-01-23 17:37:04 +0700

    menambahkan has roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -55,14 +56,5 @@ class User extends Authenticatable
         return $this->hasOne(Member::class);
     }
 
-    protected static function booted()
-{
-    static::creating(function ($user) {
-        $user->role = $user->role ?? 'admin';
-    });
-
-    static::updating(function ($user) {
-        
-    });
-}
+    protected static function booted() {}
 }
